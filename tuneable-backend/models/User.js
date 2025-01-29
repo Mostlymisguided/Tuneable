c7a190b00f8118c13d0b022369ef5dcaeb2d1502
Mostlymisguided <ed@meacock.com>
2025-01-29 19:56:39 -0400

    big ting - restructured song and user schemas - sorted population issue in songcard - parties fetching successfully and song info displaying correctly minus cover art

--- tuneable-backend/models/User.js
+++ tuneable-backend/models/User.js
@@ -5,7 +5,6 @@ const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  bio: { type: String, default: '' },
   homeLocation: {
     city: { type: String, default: null },
     country: { type: String, default: null },
