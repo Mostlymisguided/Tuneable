a05b04a3a7cf641d803920031606e839e60c3d01
Mostlymisguided <ed@meacock.com>
2025-02-09 19:58:11 -0400

    added givenName + familyName to userSchema

--- tuneable-backend/models/User.js
+++ tuneable-backend/models/User.js
@@ -4,6 +4,8 @@ const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
+  givenName:{ type: String, required: true},
+  familyNAme:{ type: String, required: true},
   password: { type: String, required: true },
   profilePic: { type: String, default: null }, // Stores URL or file path
   balance: { type: Number, default: 0 }, // New field for wallet balance
