8b9c4f5a02b6ee33abb9524be540bb7120d6214b
Mostlymisguided <ed@meacock.com>
2025-03-03 15:32:35 -0400

    added CORS headers for error responses

--- tuneable-frontend/src/index.js
+++ tuneable-frontend/src/index.js
@@ -4,8 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-console.log('Dev Token:', process.env.REACT_APP_BACKEND_URL);
-
+console.log('Backend', process.env.REACT_APP_BACKEND_URL);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
