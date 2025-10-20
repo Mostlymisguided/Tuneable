e10bd2a53e36eca09a6632a5a28bcbc8d4d07479
Mostlymisguided <ed@meacock.com>
2025-10-20 22:18:36 +0100

    Fix: Correct adminMiddleware import in reportRoutes
    Changed from destructured import to direct import to match middleware export pattern

--- tuneable-backend/routes/reportRoutes.js
+++ tuneable-backend/routes/reportRoutes.js
@@ -5,7 +5,7 @@ const Report = require('../models/Report');
 const Media = require('../models/Media');
 const User = require('../models/User');
 const authMiddleware = require('../middleware/authMiddleware');
-const { adminMiddleware } = require('../middleware/adminMiddleware');
+const adminMiddleware = require('../middleware/adminMiddleware');
 const { sendEmail } = require('../utils/emailService');
 
 // Submit a report for a media item
