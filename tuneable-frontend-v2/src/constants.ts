123d0b8bb8c685e228c43d6d9533ee962212eb84
Mostlymisguided <ed@meacock.com>
2025-11-04 16:36:26 +0000

    updated cover art fallback

--- tuneable-frontend-v2/src/constants.ts
+++ tuneable-frontend-v2/src/constants.ts
@@ -3,5 +3,5 @@
 export const DEFAULT_PROFILE_PIC = 'https://uploads.tuneable.stream/profile-pictures/default-profile.png';
 
 // Default cover art URL for media without cover art
-export const DEFAULT_COVER_ART = '/default-cover.jpg';
+export const DEFAULT_COVER_ART = 'https://uploads.tuneable.stream/cover-art/default-cover.png';
 
